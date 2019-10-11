b2b7b23d1b182a7a1cb789b6af43e59a74eb80f0
Ross <phambatoanbk@gmail.com>
2019-10-12 00:10:53 +0700

    chat nhom hoan thanh

--- public/xuly.js
+++ public/xuly.js
@@ -1,5 +1,26 @@
 var socket = io("http://localhost:3000");
 
-$(document).ready(() => {
+socket.on("server-send-rooms", (data) => {
+    $("#dsRoom").html("");
+    data.map((r) => {
+        $("#dsRoom").append("<h4 class = 'room'>" + r + "</h4");
+    });
+});
 
+socket.on("server-send-room-socket", (data) => {
+    $("#roomHientai").html(data);
+});
+
+socket.on("server-chat", (data) => {
+    $("#right").append("<div>" + data + "</div>");
+});
+
+$(document).ready(() => {
+    $("#btnTaoRoom").click(() => {
+        socket.emit("tao-room", $("#txtRoom").val());
+    });
+
+    $("#btnChat").click(() => {
+        socket.emit("user-chat", $("#txtMessage").val());
+    });
 });
--- server.js
+++ server.js
@@ -11,10 +11,24 @@ server.listen(3000);
 
 io.on("connection", (socket) => {
     console.log("co nguoi ket noi " + socket.id);
-    console.log(socket.adapter.rooms);
+    // console.log(socket.adapter.rooms);
 
     socket.on("tao-room", (data) => {
         socket.join(data);
+        socket.Phong = data;
+        //console.log(socket.adapter.rooms);
+
+        var mang = [];
+        for (r in socket.adapter.rooms) {
+            mang.push(r);
+        }
+
+        io.sockets.emit("server-send-rooms", mang);
+        socket.emit("server-send-room-socket", data);
+    });
+
+    socket.on("user-chat", (data) => {
+        io.sockets.in(socket.Phong).emit("server-chat", data);
     });
 });
 
